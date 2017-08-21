b378b297592ed4dd7ba3f6b0de72df877ea59d59
SherryH <enjoy.wind7rain@gmail.com>
2017-08-21 18:08:14 +1200

    Added basic input and lists

---
+++ ep1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () =>
+  <div>
+    <input type="search" placeholder="search" />
+    <ul>
+      <li>
+        <img src="#" role="presentation" />
+        <div>
+          <strong>name</strong>
+          <strong>email</strong>
+        </div>
+      </li>
+    </ul>
+  </div>;
+
+export default App;
