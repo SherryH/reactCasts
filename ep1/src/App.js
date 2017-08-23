eee7f4452ba5b988ee1c63f2ab7053cf061623d0
SherryH <enjoy.wind7rain@gmail.com>
2017-08-23 20:43:35 +1200

    Load dummy data from random user API

--- ep1/src/App.js
+++ ep1/src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    contacts: []
+  };
+  componentDidMount() {
+    fetch('https://api.randomuser.me/?nat=us,gb&results=3')
+      .then(response => {
+        return response.json();
+      })
+      .then(parsedRes => {
+        return parsedRes.results.map(user => ({
+          name: `${user.name.first} ${user.name.last}`,
+          email: user.email,
+          picture: user.picture.thumbnail
+        }));
+      })
+      .then(contacts => {
+        this.setState({
+          contacts
+        });
+      });
+  }
   render() {
     return (
       <div className="appContainer">
@@ -17,6 +38,20 @@ class App extends Component {
                 <strong>email</strong>
               </div>
             </li>
+            {this.state.contacts.map(contact =>
+              <li>
+                <img src={contact.picture} role="presentation" />
+                <div>
+                  <strong>
+                    {contact.name}
+                  </strong>
+                  <br />
+                  <strong>
+                    {contact.email}
+                  </strong>
+                </div>
+              </li>
+            )}
           </ul>
         </div>
       </div>
