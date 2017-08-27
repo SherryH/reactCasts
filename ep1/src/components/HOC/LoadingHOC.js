96ce2292af41544d86ff826ff3947f6d9d3358d5
SherryH <enjoy.wind7rain@gmail.com>
2017-08-27 20:31:49 +1200

    Made isEmpty generic

--- ep1/src/components/HOC/LoadingHOC.js
+++ ep1/src/components/HOC/LoadingHOC.js
@@ -7,9 +7,23 @@ import './LoadingHOC.css';
 //use curry to take in the loadingProps and then take in the WrapperComponent
 
 const LoadingHOC = loadingProps => WrappedComponent => {
+  const isEmpty = loadingProps => {
+    if (
+      loadingProps === null ||
+      loadingProps === undefined ||
+      (loadingProps.hasOwnProperty('length') && loadingProps.length === 0) ||
+      (loadingProps.constructor === 'Object' &&
+        Object.keys(loadingProps).length === 0)
+    ) {
+      return true;
+    }
+    return false;
+  };
   return class LoadingHOC extends Component {
+    // the loadingProps may not be an array.
+    // instead of checking the array length to be 0, make generic
     render() {
-      return this.props[loadingProps].length === 0
+      return isEmpty(loadingProps)
         ? <div className="loader" />
         : <WrappedComponent {...this.props} />;
     }
