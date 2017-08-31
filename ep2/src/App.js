a1e982b3d043c28622c5b24809feb55971c4d379
SherryH <enjoy.wind7rain@gmail.com>
2017-08-31 21:17:58 +1200

    Separated FACC

--- ep2/src/App.js
+++ ep2/src/App.js
@@ -3,27 +3,19 @@ import ScrollPos from './facc/ScrollPos';
 import './App.css';
 
 class App extends Component {
-  state = {
-    width: 0
-  };
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleEvent);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleEvent);
-  }
-
-  handleEvent = event => {
-    this.setState({ width: event.srcElement.body.scrollTop });
-  };
-
   render() {
     return (
-      <div className="App">
-        <div className="spacer" />
-        This is Parent App
-        <p> This can be inside a different app. Width: {this.state.width}</p>
-        <div className="spacer" />
+      <div>
+        This is the Parent App
+        <ScrollPos>
+          {width => (
+            <div className="round">
+              Parent:Here, the shape and style of what is passed in is
+              controlled by Parent
+              {width}
+            </div>
+          )}
+        </ScrollPos>
       </div>
     );
   }
--- ep2/src/facc/ScrollPos.js
+++ ep2/src/facc/ScrollPos.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import '../App.css';
 
-const ScrollPos = () => {
-  return <p> This can be inside a different app. Width: {this.state.width}</p>;
-};
+class ScrollPos extends Component {
+  static PropTypes = {
+    children: PropTypes.func.isRequired
+  };
+
+  state = {
+    width: 0
+  };
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleEvent);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleEvent);
+  }
+
+  handleEvent = event => {
+    this.setState({ width: event.srcElement.body.scrollTop });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="spacer" />
+        This is Child App
+        <p> Child:This can be inside a different app. Width: </p>
+        {this.props.children(this.state.width)}
+        <div className="spacer" />
+      </div>
+    );
+  }
+}
 
 export default ScrollPos;
